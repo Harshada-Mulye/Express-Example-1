e0a80376fd6c6534d3eba8ae7cd13ac4cf1d9e81
Harshada-Mulye <mulyeharshada@gmail.com>
2021-04-19 13:07:40 +0200

    CRUD added

--- routes/tools.js
+++ routes/tools.js
@@ -22,4 +22,69 @@ router.get('/', async (req, res) => {
 	})
 	res.send(items)
 })
+
+router.get('/:id', async (req, res) => {
+	const id=req.params.id
+	const docRef=await db.collection('tools').doc(id).get()
+	if(!docRef.exists)
+	{
+		res.status(404).send('Tool does not exist')
+		return
+	}
+	const data=docRef.data()
+	res.send(data)
+})
+
+// POST /tools
+router.post('/', async (req, res) => {
+	// OBS! Måste installera express.json för att detta ska fungera
+	const object = req.body
+
+	if( !isToolsObject(object) ) {
+		res.sendStatus(400)
+		return
+	}
+
+	const docRef = await db.collection('tools').add(object)
+	res.send(docRef.id)
+})
+
+// PUT /tools/:id
+router.put('/:id', async (req, res) => {
+	const object = req.body
+	const id = req.params.id
+
+	if( !object || !id ) {
+		res.sendStatus(400)
+		return
+	}
+
+	// Vi kan kontrollera om det finns ett doc som matchar id i databasen. Den här koden godkänner id som inte matchar, och lägger till ett nytt doc i databasen.
+
+	const docRef = db.collection('tools').doc(id)
+	await docRef.set(object, { merge: true })
+	res.sendStatus(200)
+})
+
+function isToolsObject(maybeObject) {
+	// Pratigt, men kanske mera lättläst. Kan göras mer kompakt
+	if( !maybeObject )
+		return false
+	else if( !maybeObject.name || !maybeObject.price )
+		return false
+
+	return true
+}
+router.delete('/:id', async (req, res) => {
+	const id = req.params.id
+
+	if( !id ) {
+		res.sendStatus(400)
+		return
+	}
+
+	await db.collection('tools').doc(id).delete()
+	res.sendStatus(200)
+})
+
 module.exports=router
